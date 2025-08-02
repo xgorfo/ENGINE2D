f179d2259adfcc8b4c612e4c4433d86fc649991b
Anfisa Vinogradova <xgorfo@gmail.com>
2025-08-02 11:00:21 +0300

    Update SFML, init current files

---
+++ build/CMakeFiles/MainEngine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MainEngine.dir/main.cpp.o"
+  "CMakeFiles/MainEngine.dir/main.cpp.o.d"
+  "MainEngine"
+  "MainEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MainEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
